e341ef3f6a8f198ae567967f24e99b036c8fc894
alexeve3967 <alexeve3967@gmail.com>
2017-02-12 21:00:56 -0500

    Added Alex to the method - test

--- app/src/main/java/com/example/reehams/goodreads/WelcomeActivity.java
+++ app/src/main/java/com/example/reehams/goodreads/WelcomeActivity.java
@@ -17,6 +17,7 @@ public class WelcomeActivity extends AppCompatActivity {
 
     private void test() {
         System.out.println("Rahul");
-        System.out.println("Reeham"); 
+        System.out.println("Reeham");
+        System.out.println("Alex");
     }
 }
