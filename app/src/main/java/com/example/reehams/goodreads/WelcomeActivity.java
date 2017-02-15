fea1fba53d1bc7bdf11f78ecc8974f61224b7862
Rahul Kooverjee <rahulkooverjee@gmail.com>
2017-02-14 19:13:14 -0500

    Trying to get search page working

--- app/src/main/java/com/example/reehams/goodreads/WelcomeActivity.java
+++ app/src/main/java/com/example/reehams/goodreads/WelcomeActivity.java
@@ -3,6 +3,7 @@ package com.example.reehams.goodreads;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.facebook.CallbackManager;
@@ -49,6 +50,16 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         Intent i = new Intent(this, AboutUs.class);
+        startActivityForResult(i, 1);
+    }
+
+    /**
+     * Handle button presses by starting the game
+     * @param view
+     */
+    public void onButtonPress(View view) {
+        Intent i = new Intent(this, MovieSearch.class);
+        // i.putExtra("numPoints", numPoints);
         startActivityForResult(i, 1);
     }
 }
