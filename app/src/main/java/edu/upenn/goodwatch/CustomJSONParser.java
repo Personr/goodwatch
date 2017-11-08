95b69144ab6947ea4d7f57a4df2570beef46038d
Your Name <zormert@seas.upenn.edu>
2017-11-07 20:41:20 -0500

    Added ability browse movies by genre,keyword or actor

--- app/src/main/java/edu/upenn/goodwatch/CustomJSONParser.java
+++ app/src/main/java/edu/upenn/goodwatch/CustomJSONParser.java
@@ -1,8 +1,13 @@
 package edu.upenn.goodwatch;
 
 
-//Parses badly formatted Json per request
+import android.util.Log;
+
+import java.util.ArrayList;
+
+//Custom parser utility class
 public class CustomJSONParser {
+
 
     public static String getYear(String input) {
         String regex = "\"year\":\"";
@@ -67,5 +72,77 @@ public class CustomJSONParser {
         int endIndex = temp.indexOf('"');
         return temp.substring(0, endIndex);
     }
+
+    public static int getID(String input) {
+        String regex = "\"id\":";
+        int index = input.indexOf(regex);
+        String temp = input.substring(index + regex.length(), input.length());
+        int endIndex = temp.indexOf('"');
+        return Integer.parseInt(temp.substring(0, endIndex - 1));
+    }
+
+    public static ArrayList<String> getMovieList(String input) {
+        ArrayList<String> movieList = new ArrayList<String>();
+        while (true) {
+            String regex = "\"original_title\":\"";
+            int index = input.indexOf(regex);
+            if (index < 0) break;
+            input = input.substring(index + regex.length(), input.length());
+            int endIndex = input.indexOf('"');
+            String movie = input.substring(0, endIndex);
+            movieList.add(movie);
+            input = input.substring(endIndex, input.length());
+            if (movieList.size() >= 10) break;
+        }
+        return movieList;
+    }
+
+    public static ArrayList<String> getMovieListKeyword(String input) {
+        ArrayList<String> movieList = new ArrayList<String>();
+        while (true) {
+            String regex = "\"title\":\"";
+            int index = input.indexOf(regex);
+            if (index < 0) break;
+            input = input.substring(index + regex.length(), input.length());
+            int endIndex = input.indexOf('"');
+            String movie = input.substring(0, endIndex);
+            movieList.add(movie);
+            input = input.substring(endIndex, input.length());
+            if (movieList.size() >= 10) break;
+        }
+        return movieList;
+    }
+
+    public static int findGenreFromId(String input, String targetGenre) {
+        //Parse the string till found
+        int targetId = -1;
+        while (true) {
+            String regex = "\"id\":";
+            int index = input.indexOf(regex);
+            if (index < 0) break;
+            input = input.substring(index + regex.length(), input.length());
+            int endIndex = input.indexOf(',');
+            int ID = Integer.parseInt(input.substring(0, endIndex));
+            Log.v(null, "Herez");
+            Log.v(null, ID + "");
+
+            input = input.substring(endIndex, input.length());
+
+            //Now Get the genre to see if this is the target
+            regex = "\"name\":\"";
+            index = input.indexOf(regex);
+            if (index < 0) break;
+            input = input.substring(index + regex.length(), input.length());
+            endIndex = input.indexOf('"');
+            String name = input.substring(0, endIndex);
+
+            if (name.equals(targetGenre)) {
+                targetId = ID;
+                break;
+            }
+        }
+        return targetId;
+    }
+
 
 }
