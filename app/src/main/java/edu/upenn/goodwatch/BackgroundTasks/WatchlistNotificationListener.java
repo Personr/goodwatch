534508ed13acd67c1dabc908f185b33faff6ea69
Alex Thurston <ajthurston@gmail.com>
2017-11-14 09:41:01 -0500

    Have Listener pop the toast directly

--- app/src/main/java/edu/upenn/goodwatch/BackgroundTasks/WatchlistNotificationListener.java
+++ app/src/main/java/edu/upenn/goodwatch/BackgroundTasks/WatchlistNotificationListener.java
@@ -1,5 +1,11 @@
 package edu.upenn.goodwatch.BackgroundTasks;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -7,6 +13,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.concurrent.CountDownLatch;
+
+import edu.upenn.goodwatch.HomeActivity;
 
 /**
  * Created by Alex on 11/11/17.
@@ -18,6 +26,8 @@ public class WatchlistNotificationListener implements ValueEventListener {
     private StringBuffer sb;
     private String title;
     private CountDownLatch latch;
+    //TODO pass in activity
+    private Activity callingActivity;
 
     public WatchlistNotificationListener(String movieId, String title, StringBuffer sb, CountDownLatch latch) {
         this.movieId = movieId;
@@ -35,6 +45,13 @@ public class WatchlistNotificationListener implements ValueEventListener {
         // Otherwise, notify user of changes
         } else {
             sb.append(title).append("\n");
+            callingActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    String msg = String.format("New activity for %s in your watchlist!", title);
+                    Toast.makeText(callingActivity, msg, Toast.LENGTH_SHORT).show();
+                }
+            });
         }
         // Count down so that the WatchlistNotificationManager knows this is done
         latch.countDown();
